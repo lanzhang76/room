4a548491a6bae0eecafb306139c338cb57152a59
Lan Zhang <zhany016@newschool.edu>
2020-04-27 15:31:12 -0400

    main

--- public/main.js
+++ public/main.js
@@ -1,12 +1,13 @@
-var socket = io().connect("oursever");
+var socket = io();
 
 socket.emit('room', window.location.pathname);
+
 socket.on('update', (msg) => {
-    $('#messages').append($('<span>').text(msg));
+    $('#messages').append($('<span>').text(msg)); //append text
 })
 
 socket.on("contentBroadcast", (msg) => {
-    $('#messages').append(msg);
+    $('#messages').append(msg); //append dom element
 });
 
 
