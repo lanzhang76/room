919ab4540f25d29f6d701eb890cb72610b0669cb
Anna <annagarbier@gmail.com>
2020-04-28 08:05:26 -0400

    add draft messages

--- components/contentMSG.js
+++ components/contentMSG.js
@@ -1,11 +1,11 @@
-var r = require("./random");
+var r = require('./random');
 
 exports.content = (user, content) => {
-    var user = user;
-    var student = content.name;
-    var project = content.project;
-    var url = content.url;
+  var user = user;
+  var student = content.name;
+  var project = content.project;
+  var url = content.url;
 
-    var msg = `<span>${user} is viewing</span> <a href="${url}">${project}</a> <span>by ${student}.</span>`
-    return msg
-}
+  var msg = `<span>${user} is viewing</span> <a href="${url}">${project}</a> <span>by ${student}.</span>`;
+  return msg;
+};
