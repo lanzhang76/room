e7cf79cf73af9780facecd1d50c47f88af6239b5
czhowl <czhhzc94@gmail.com>
2020-04-30 11:18:40 -0400

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,39 +1,31 @@
 var uuid = localStorage.getItem('mfadtuuid');
-if(uuid == null){
+if (uuid == null) {
     uuid = Math.random().toString(24) + new Date();
     localStorage.setItem('mfadtuuid', uuid);
 }
 var socket = io({ query: { uuid: uuid } });
 
+contentView();
 socket.emit('query-log', 0);
 socket.emit('page', window.location.pathname);
-contentView();
 
 socket.on('update', (msg) => {
-    if (Object.keys(msg).length == 1) {
-        $('#messages').append($('<span>').text(msg.sen)); //append text
-    } else {
-        console.log(msg)
-        var url = `${window.location.hostname}:5000${msg.path}`
-        console.log(url);
-        var sent = `<a href=${url}><span>${msg.name}</span></a>`
-        var output = `<span>${msg.sen.replace(msg.name, sent)}</span>`
-        $('#messages').append(output);
-    }
+    parseLink(msg);
+    $("#bulletin_board").animate({ scrollTop: $("#bulletin_board").prop('scrollHeight') }, 300, 'linear');
 })
 
 socket.on("requested", (msg) => {
     msg.forEach(element => {
-        $('#messages').prepend($('<p>').text(element.text)); //append dom element
+        parseLink(msg);
     });
+    $("#bulletin_board").animate({ scrollTop: $("#bulletin_board").prop('scrollHeight') }, 300, 'linear');
 });
 
 var offset = 10;
-$('#load').click(()=>{
+$('#load').click(() => {
     socket.emit('query-log', offset);
     offset = offset + 10;
 })
-
 
 function contentView() {
     // Push project name, student name, project path to the server
@@ -46,5 +38,18 @@ function contentView() {
         console.log(window.location.pathname)
         project.push(window.location.pathname);
         socket.emit("contentView", project);
+    }
+}
+
+function parseLink(){
+    if (Object.keys(msg).length == 1) {
+        $('#messages').append($('<p>').text(msg.sen)); //append text
+    } else {
+        console.log(msg)
+        var url = `${window.location.hostname}:5000${msg.path}`
+        console.log(url);
+        var sent = `<a href=${url}><span>${msg.name}</span></a>`
+        var output = `<p>${msg.sen.replace(msg.name, sent)}</p>`
+        $('#messages').append(output);
     }
 }
