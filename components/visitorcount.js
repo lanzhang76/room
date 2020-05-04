553ae132a7c03af444f641b75986bd6f91705e0b
Anna <annagarbier@gmail.com>
2020-05-04 10:31:01 -0400

    modify message for visitor count

--- components/visitorcount.js
+++ components/visitorcount.js
@@ -1,26 +1,26 @@
 var rand = require('./random');
 
 exports.totalcount = (num) => {
-    var count_sent = '';
-    var messages = (num_users) => {
-        var message = rand.rand([
-            `It’s a lively day at Hindsight 2020. ${num_users} at the show!`,
-            `Things are really picking up! ${num_users} gather.`,
-            `Come one, come all! ${num_users} meet up at Hindsight 2020.`,
-            `Friends, families, strangers, neighbors...${num_users} art enthusiasts fill the space.`,
-        ])
-        return messgae
-    }
+  var count_sent = '';
+  var messages = (num_users) => {
+    var message = rand.rand([
+      `It’s a lively day at Hindsight 2020. ${num_users} people at the show!`,
+      `Things are really picking up! ${num_users} people gather.`,
+      `Come one, come all! ${num_users} people meet up at Hindsight 2020.`,
+      `Friends, families, strangers, neighbors...${num_users} art enthusiasts fill the space.`,
+    ]);
+    return messgae;
+  };
 
-    if (num == 5) {
-        count_sent = messaged(num);
-    } else if (num == 10) {
-        count_sent = messaged(num);
-    } else if (num == 20) {
-        count_sent = messaged(num);
-    } else if (num == 50) {
-        count_sent = messaged(num);
-    }
-    // Should return '' if no special event happens
-    return count_sent;
-}
+  if (num == 5) {
+    count_sent = messaged(num);
+  } else if (num == 10) {
+    count_sent = messaged(num);
+  } else if (num == 20) {
+    count_sent = messaged(num);
+  } else if (num == 50) {
+    count_sent = messaged(num);
+  }
+  // Should return '' if no special event happens
+  return count_sent;
+};
