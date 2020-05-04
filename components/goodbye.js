932797c01b7d631501e5ae08fb864072b8bc6ff3
Anna <annagarbier@gmail.com>
2020-05-04 10:23:37 -0400

    fix typo

--- components/goodbye.js
+++ components/goodbye.js
@@ -6,7 +6,7 @@ exports.goodbye = () => {
     'still thinking about the last piece they saw.',
     'with a new appreciation for design...and pockets full of macarons (shhh).',
     'ready to tell their friends all about it.',
-    "to tell their family to stop by next time they're in town",
+    "to tell their family to stop by next time they're in town.",
     'feeling excited.',
     'ready to start their own project.',
   ];
