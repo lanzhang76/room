167706bab056f6800873805cca72d50ab61e28c6
Anna <annagarbier@gmail.com>
2020-05-04 10:23:09 -0400

    add more goodbye messages

--- components/goodbye.js
+++ components/goodbye.js
@@ -1,6 +1,14 @@
-var r = require("./random");
+var r = require('./random');
 
 exports.goodbye = () => {
-    var farewell = ['feeling inspired.', "still thinking about the last piece he saw."]
-    return r.rand(farewell);
-}
+  var farewell = [
+    'feeling inspired.',
+    'still thinking about the last piece they saw.',
+    'with a new appreciation for design...and pockets full of macarons (shhh).',
+    'ready to tell their friends all about it.',
+    "to tell their family to stop by next time they're in town",
+    'feeling excited.',
+    'ready to start their own project.',
+  ];
+  return r.rand(farewell);
+};
